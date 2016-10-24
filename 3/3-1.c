6d8a1392dbac62bda08532265414f01ecd368e14
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2016-10-24 15:35:05 +0300

    Added measuring run-time difference to 3-1

--- 3/3-1.c
+++ 3/3-1.c
@@ -8,30 +8,86 @@
 
 #include <assert.h>
 #include <stdio.h>
+#include <time.h>
 
-#define LIM 6
+#define LIM 20000
+#define N -1
 
-int binary_search(int elem, int ar[], int len);
+int binary_search(int elem, int ar[], int len);  /* K&R */
+int binary_search2(int elem, int ar[], int len); /* New */
 
 int main(void)
 {
-        int ar[LIM] = { 1, 2, 3, 4, 5, 6 };
+        int data[LIM];
+        int found;
+        int i;
+        clock_t time_taken;
 
-        assert(binary_search(1, ar, LIM) == 0);
-        assert(binary_search(2, ar, LIM) == 1);
-        assert(binary_search(3, ar, LIM) == 2);
-        assert(binary_search(4, ar, LIM) == 3);
-        assert(binary_search(5, ar, LIM) == 4);
-        assert(binary_search(6, ar, LIM) == 5);
-        assert(binary_search(0, ar, LIM) == -1);
-        assert(binary_search(7, ar, LIM) == -1);
-        assert(binary_search(8, ar, LIM) == -1);
-        puts("success");
+        /* Initialize array with data */
+        for (i = 0; i < LIM; ++i)
+                data[i] = i;
+
+        /*  Output: approximation of time taken for 100,000 iterations
+            of binary_search() */
+        time_taken = 0;
+        for (i = 0, time_taken = clock(); i < 100000; ++i) {
+                found = binary_search(N, data, LIM);
+        }
+        time_taken = clock() - time_taken;
+
+        /* Print results */
+        if (found < 0)
+                printf("\nElement %d not found.\n", N);
+        else
+                printf("\nElement %d found at index %d.\n", N, found);
+        printf("binary_search() took %.0lf clocks (%.2lf seconds)\n",
+                        (double) time_taken,
+                        (double) time_taken / CLOCKS_PER_SEC);
+
+        /*  Output: approximation of time taken for 100,000 iterations
+            of binary_search2() */
+        time_taken = 0;
+        for (i = 0, time_taken = clock(); i < 100000; ++i) {
+                found = binary_search2(N, data, LIM);
+        }
+        time_taken = clock() - time_taken;
+
+        /* Print results */
+        if (found < 0)
+                printf("\nElement %d not found.\n", N);
+        else
+                printf("\nElement %d found at index %d.\n", N, found);
+        printf("binary_search2() took %.0lf clocks (%.2lf seconds)\n\n",
+                        (double) time_taken,
+                        (double) time_taken / CLOCKS_PER_SEC);
 
         return 0;
 }
 
+/* K&R */
 int binary_search(int elem, int ar[], int len)
+{
+        int low;
+        int high;
+        int mid;
+
+        low = 0;
+        high = len - 1;
+
+        while (low <= high) {
+                mid = (low + high) / 2;
+                if (ar[mid] == elem)
+                        return mid;
+                else if (ar[mid] < elem)
+                        low = mid + 1;
+                else
+                        high = mid - 1;
+        }
+        return -1;
+}
+
+/* New */
+int binary_search2(int elem, int ar[], int len)
 {
         int low;
         int high;
